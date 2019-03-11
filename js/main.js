c490fc683d15d112607adf510f1e3ddf116044b0
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2019-03-11 21:41:01 +0200

    Added adaptive welcome page.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,44 @@
 $(document).ready(function () {
 
+    $("nav:first").height(20);
+
+    function setHeight(){
+        let body = $(window).height();
+        let nav = $("nav:first");
+        let jumbotron =  $( ".jumbotron" ).first();
+
+        if(isCompressed){
+            nav.height(body * 0.1);
+            jumbotron.height((body * 0.9) - 20);
+
+            console.log(body * 0.1);
+            console.log((body * 0.9) - 20);
+            console.log('-------------------');
+        }else {
+            nav.height(body * 0.1);
+            jumbotron.height((body * 0.9) - 4);
+
+            console.log(body * 0.1);
+            console.log((body * 0.9) - 4);
+            console.log('-------------------');
+        }
+    }
+
+    let isCompressed = false;
+
     $(window).on('scroll', function () {
         if ($(window).scrollTop() >= 20) {
             $('.navbar').addClass('compressed');
+            isCompressed = true;
         } else {
             $('.navbar').removeClass('compressed');
+            isCompressed = false;
         }
     });
 
-    /*var navbarOffset = $('.navbar').offset().top;
-
-    $(window).on('scroll', function () {
-
-        if ( $(window).scrollTop() >= navOffset ) { // if the navbar offset equal or bigger than user scrolling, make it fixed
-            $('.navbar').addClass('navbar-fixed-top');
-
-        } else {// if not, return it to its initial state
-            $('.navbar').removeClass('navbar-fixed-top');
-        }
-
-    });*/
+    $( window ).resize(function() {
+        console.log($(window).height());
+        //setHeight();
+    });
 
 });
