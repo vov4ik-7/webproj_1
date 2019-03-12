0a1744f59e2e31585647598ad7aa78b7c032837e
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2019-03-12 20:09:13 +0200

    Fixed welcome page.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,21 @@
 $(document).ready(function () {
-
-    $("nav:first").height(20);
+    setHeight();
 
     function setHeight(){
         let body = $(window).height();
         let nav = $("nav:first");
         let jumbotron =  $( ".jumbotron" ).first();
 
-        if(isCompressed){
-            nav.height(body * 0.1);
-            jumbotron.height((body * 0.9) - 20);
+        jumbotron.height(body - nav.height() - 16);
 
-            console.log(body * 0.1);
-            console.log((body * 0.9) - 20);
-            console.log('-------------------');
-        }else {
-            nav.height(body * 0.1);
-            jumbotron.height((body * 0.9) - 4);
-
-            console.log(body * 0.1);
-            console.log((body * 0.9) - 4);
-            console.log('-------------------');
-        }
+        console.log(body);
+        console.log(nav.height());
+        console.log(jumbotron.height());
+        console.log('-----------------------');
     }
 
-    let isCompressed = false;
-
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() >= 20) {
-            $('.navbar').addClass('compressed');
-            isCompressed = true;
-        } else {
-            $('.navbar').removeClass('compressed');
-            isCompressed = false;
-        }
-    });
-
     $( window ).resize(function() {
-        console.log($(window).height());
-        //setHeight();
+        setHeight();
     });
 
 });
